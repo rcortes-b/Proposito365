da84adf0f2113dbf6cbfac787a681172556c4974
rcortes-b <raulcortes.dev@gmail.com>
2025-12-16 22:41:21 +0100

    fixes done --- app running

--- cleanup.sh
+++ cleanup.sh
@@ -7,5 +7,9 @@ docker stop propositos_backend_container
 docker rm propositos_backend_container
 docker image rm -f docker-backend
 
+docker stop propositos_nginx_container
+docker rm propositos_nginx_container
+docker image rm -f docker-nginx_server
+
 docker volume rm docker_db_data
 docker network rm propositos_network
